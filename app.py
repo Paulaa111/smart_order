004237e3a91f7e5e87dab808c8cb79371995b0b8
Paulaa111 <152186017+Paulaa111@users.noreply.github.com>
2026-03-09 08:55:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -469,7 +469,7 @@ if not st.session_state.submitted:
             
             try:
                 client = get_gspread_client()
-                client.open("Baza_Zamowien").worksheet("Arkusz1").append_row(dane_do_zapisu, value_input_option='USER_ENTERED')        
+                client.open("Baza_Zamowien").worksheet("Arkusz1").append_rows([dane_do_zapisu], value_input_option='USER_ENTERED')        
                 
                 # ZAPISUJEMY WSZYSTKIE DANE DO SESJI
                 st.session_state.order_data = {
@@ -534,4 +534,5 @@ else:
         for key in ["submitted", "order_id", "order_data"]: st.session_state.pop(key, None)
         st.rerun()
    
+
 
