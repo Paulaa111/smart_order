129417eeb79ea366234aed5490f9d9e1123e0641
Paulaa111 <152186017+Paulaa111@users.noreply.github.com>
2026-03-23 13:39:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.set_page_config(
     page_title="Sweet Order · Cukiernia",
     page_icon="🎂",
     layout="wide",
-    initial_sidebar_state="collapsed",
+    initial_sidebar_state="expanded",
 )
 
 # ─── GLOBAL CSS ───────────────────────────────────────────────────────────────
